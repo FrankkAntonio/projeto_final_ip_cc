e2bcda7456e4210f04f62b3adba500f2b807e165
Frankk Antonio <frankkantonio69@gmail.com>
2025-05-14 21:43:21 -0300

    Add files via upload

---
+++ projeto_final_ip_cc/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def ola():
+   # return ('<h1>Olá, Mundo!</h1>')
+
+    return render_template('index.html')
+
+@app.route('/sobre.html')
+def equipes():
+
+    return render_template('sobre.html')
+
+
+
+app.run()
+
